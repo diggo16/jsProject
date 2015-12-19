f1977f2815c8693ed6f865bbee5e54654dc9ffbd
Christoffer Holmgren <christoffer.holmgren@gmail.com>
2015-12-19 22:14:33 +0100

    Changed some code, and more.
    - Removed syles.css
    - defaultValue on username input makes smaller edits easier.
    - Changed the addMessage method to look and feel nicer, but depending on what formatting you actually want, it might be too much.
    - Removed index.js.

--- public_html/src/components/settings.js
+++ public_html/src/components/settings.js
@@ -4,42 +4,39 @@ var React = require('react'),
     actions = require('../actions');
 /*
  * The react class that renders the message list and add button
-*/
+ */
 var MessageList = React.createClass({
     propTypes: {
-		changeSettings: ptypes.func.isRequired
+        changeSettings: ptypes.func.isRequired
     },
-	// Handle the add button when clicked
-	handleClick(e) {
-		// collect the text from the text input
-		var username = this.refs.username;
-		var usernameValue = username.value.trim();
-		// Send the text to the onAddClick function that adds it to the list		
-		this.props.changeSettings(usernameValue);
-		// Reset the input field
-		username.value = '';
-	},
-	// Render the message list, text input field and add button
-    render: function(){
+    // Handle the add button when clicked
+    handleClick(e) {
+        // collect the text from the text input
+        var username = this.refs.username.value.trim();
+        // Send the text to the onAddClick function that adds it to the list
+        this.props.changeSettings(username);
+    },
+    // Render the message list, text input field and add button
+    render: function () {
         return (
-			<div>
-			<p>Settings</p>
-			<p>Author:<input type='text' ref='username' /></p>
-			<button onClick={(e) => this.handleClick(e)}>
-			 Change
-			</button>
-			</div>
+            <div>
+                <p>Settings</p>
+                <p>Name:<input type='text' ref='username' defaultValue={this.props.author}/></p>
+                <button onClick={(e) => this.handleClick(e)}>
+                    Change
+                </button>
+            </div>
         );
     }
 });
 // Transform the message state to props
-var mapStateToProps = function(state){
+var mapStateToProps = function (state) {
     return state.settings;
 };
 // Connect onAddClick function to the messagelistreducer
-var mapDispatchToProps = function(dispatch){
+var mapDispatchToProps = function (dispatch) {
     return {
-		changeSettings: function(username){
+        changeSettings: function (username) {
             dispatch(actions.changeSettings(username));
         }
     }
